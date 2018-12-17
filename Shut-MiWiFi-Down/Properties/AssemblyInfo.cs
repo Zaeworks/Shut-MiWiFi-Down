8f514df7894b8840e3d0c34a85816dc30d5f9c4e
Zae <zaevi@live.com>
2018-12-18 00:06:26 +0800

    init project

---
+++ Shut-MiWiFi-Down/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Shut-MiWiFi-Down")]
+
+[assembly: AssemblyProduct("Shut-MiWiFi-Down")]
+[assembly: AssemblyCopyright("Copyright © zaevi 2018")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c186fa9e-40c8-4459-b4eb-c2b8aaff780b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
